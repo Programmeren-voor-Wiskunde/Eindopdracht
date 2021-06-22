96f93f996e8d953afa7ff16822beeccb8fefd319
lgrvanlieshout <lgrlevi@gmail.com>
2021-06-22 16:44:29 +0200

    Update Mogelijke_combinaties.py

--- Mogelijke_combinaties.py
+++ Mogelijke_combinaties.py
@@ -1,14 +1,19 @@
 
 import copy
 
-def mogelijke_combinaties():
+def mogelijke_combinaties(kaartendek: list):
+    """
+    Deze functie geeft alle mogelijke combinaties van drie verschillende kaarten 
+    uit een verzameling van twaalf kaarten, waarbij de volgorde niet uitmaakt.
+
+    """
     mogelijkheden = []
-    for i in range(1,13):
-        combinatie = [i]
-        for j in range(i+1,13):
-            combinatie.append(j)
-            for k in range(j+1,13):
-                combinatie.append(k)
+    for i in range(12):
+        combinatie = [kaartendek[i]]
+        for j in range(i+1,12):
+            combinatie.append(kaartendek[j])
+            for k in range(j+1,12):
+                combinatie.append(kaartendek[k])
                 if combinatie not in mogelijkheden and len(combinatie)==3:
                     mogelijkheden.append(copy.copy(combinatie))
                     combinatie.pop()
