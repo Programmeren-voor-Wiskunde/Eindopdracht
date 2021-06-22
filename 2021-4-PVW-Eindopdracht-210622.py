dbba9aa2864ece5d3c647a16b5914dc1be11f8db
Jelger van Haskera <jelger.van.haskera@outlook.com>
2021-06-22 16:55:30 +0200

    Update 2021-4-PVW-Eindopdracht-210622.py

--- 2021-4-PVW-Eindopdracht-210622.py
+++ 2021-4-PVW-Eindopdracht-210622.py
@@ -5,9 +5,10 @@ Created on Tue Jun 22 11:43:47 2021
 @author: Levi G.R. van Lieshout & Jelger J. van Haskera
 
 Classes: Kaart
-Functions: 
+Functions, mogelijke_combinaties(), unit_tests(): 
 """
 
+import copy
 
 class Kaart:
     """Represents a cart of the game Set"
@@ -36,21 +37,6 @@ class Kaart:
                 return matchings[i]
         return True
 
-"""Unit Tests:""" 
-#is_set, expected: True 
-kaart1=Kaart(0,1,2,1)
-kaart2=Kaart(0,2,3,3)
-kaart3=Kaart(0,3,1,4)
-
-print("Unit test: "+str(kaart1)+","+str(kaart2)+","+str(kaart3)+" "+ "Expected: True, return Falue:"+str(kaart1.is_set(kaart2,kaart3)))
-      
-#is_set, expected: False 
-kaart4=Kaart(1,1,2,1)
-kaart5=Kaart(0,2,2,3)
-kaart6=Kaart(0,1,1,3)
-print("Unit test: "+str(kaart4)+","+str(kaart5)+","+str(kaart6)+" "+ "Expected: False, return Falue:"+str(kaart4.is_set(kaart5,kaart6)))
-
-import copy
 
 def mogelijke_combinaties():
     """
@@ -71,3 +57,32 @@ def mogelijke_combinaties():
             combinatie.pop()
     print(mogelijkheden)
     return mogelijkheden
+
+def unit_tests():
+    """Unit Tests: worden alleen geprint indien returnvalue onjuist is""" 
+    #is_set, expected: True 
+    kaart1, kaart2, kaart3=Kaart(0,1,2,1), Kaart(0,2,3,3), Kaart(0,3,1,4)
+    kaart4, kaart5, kaart6=Kaart(1,0,2,1), Kaart(2,0,3,3), Kaart(3,0,1,4)
+    kaart7, kaart8, kaart9=Kaart(1,2,0,1), Kaart(2,3,0,3), Kaart(3,1,0,4)
+    kaart10, kaart11, kaart12=Kaart(1,2,1,0), Kaart(2,3,3,0), Kaart(3,1,4,0)
+    
+    if kaart1.is_set(kaart2,kaart3)==False:
+        print("Unit test 1: "+str(kaart1)+","+str(kaart2)+","+str(kaart3)+" "+ "Expected: True, return Falue:"+str(kaart1.is_set(kaart2,kaart3)))
+    if kaart4.is_set(kaart5,kaart6)==False:
+        print("Unit test 2: "+str(kaart4)+","+str(kaart5)+","+str(kaart6)+" "+ "Expected: True, return Falue:"+str(kaart4.is_set(kaart5,kaart6)))
+    if kaart7.is_set(kaart8,kaart9)==False:
+        print("Unit test 3: "+str(kaart7)+","+str(kaart8)+","+str(kaart9)+" "+ "Expected: True, return Falue:"+str(kaart7.is_set(kaart8,kaart9)))
+    if kaart10.is_set(kaart11,kaart12)==False:
+        print("Unit test 4: "+str(kaart10)+","+str(kaart11)+","+str(kaart12)+" "+ "Expected: True, return Falue:"+str(kaart10.is_set(kaart11,kaart12)))      
+    
+    #is_set, expected: False 
+    kaart13, kaart14, kaart15=Kaart(1,1,2,1), Kaart(0,2,2,3), Kaart(0,1,1,3)
+    kaart16, kaart17, kaart18=Kaart(2,1,3,3), Kaart(2,1,1,2), Kaart(1,2,1,2)
+    kaart19, kaart20, kaart21=Kaart(3,2,1,1), Kaart(2,3,3,3), Kaart(3,3,1,1)
+    
+    if kaart13.is_set(kaart14,kaart15)==True:
+        print("Unit test: "+str(kaart13)+","+str(kaart14)+","+str(kaart15)+" "+ "Expected: False, return Falue:"+str(kaart13.is_set(kaart14,kaart15)))
+    if kaart16.is_set(kaart17,kaart18)==True:
+        print("Unit test: "+str(kaart16)+","+str(kaart17)+","+str(kaart18)+" "+ "Expected: False, return Falue:"+str(kaart16.is_set(kaart17,kaart18)))
+    if kaart19.is_set(kaart20,kaart21)==True:
+        print("Unit test: "+str(kaart19)+","+str(kaart20)+","+str(kaart21)+" "+ "Expected: False, return Falue:"+str(kaart19.is_set(kaart20,kaart21)))
