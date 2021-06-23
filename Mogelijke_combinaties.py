a4ea0d64e209fc08be14497a6c2d33de2b019d48
lgrvanlieshout <lgrlevi@gmail.com>
2021-06-23 09:34:20 +0200

    Update Mogelijke_combinaties
    Ik heb i.p.v. "deck", als positional argument "set_of_twelve" gebruikt, want dat is duidelijker.

--- Mogelijke_combinaties.py
+++ Mogelijke_combinaties.py
@@ -1,7 +1,6 @@
-
 import copy
 
-def possible_combinations(deck: list):
+def possible_combinations(set_of_twelve: list):
     """
     This function returns all the possible combinations of three different cards 
     from a set of twelve cards, in which the order of the cards doesn't matter.
@@ -12,14 +11,13 @@ def possible_combinations(deck: list):
     """
     possibilities = []
     for i in range(12):
-        combination = [deck[i]]
+        combination = [set_of_twelve[i]]
         for j in range(i+1,12):
-            combination.append(deck[j])
+            combination.append(set_of_twelve[j])
             for k in range(j+1,12):
-                combination.append(deck[k])
+                combination.append(set_of_twelve[k])
                 if combination not in possibilities and len(combination)==3:
                     possibilities.append(copy.copy(combination))
                     combination.pop()
             combination.pop()
-    print(possibilities)
     return possibilities
