3b2fa211e053a0b67bf7b6f88a3274052b6dd0fe
lgrvanlieshout <lgrlevi@gmail.com>
2021-06-25 13:47:32 +0200

    Update Class.py
    Update on Class function descriptions and new function

--- Class.py
+++ Class.py
@@ -21,6 +21,17 @@ class Kaart:
         return("("+str(self.color)+","+str(self.number)+","+str(self.filling)+","+str(self.shape)+")")
         
     def is_set(self,other1,other2):
+        """
+        This method checks whether the given cards form a Set.
+        
+        Input:
+            self = Card object
+            other1 = Card object
+            other2 = Card object
+        Output:
+            True if the cards form a Set
+            False if the cards don't form a Set
+        """
         matchings={"color": False,"number": False, "filling": False, "shape": False}
         matchings["color"]=(self.color==other1.color and other1.color==other2.color) or (self.color!=other1.color and other1.color!=other2.color and self.color!=other2.color)
         matchings["number"]=(self.number==other1.number and other1.number==other2.number) or (self.number!=other1.number and other1.number!=other2.number and self.number!=other2.number)
@@ -30,3 +41,46 @@ class Kaart:
             if matchings[i]==False:
                 return matchings[i]
         return True
+    
+    def file_name(self):
+        """ 
+        Given a card, this method returns the name of the associated image file of the card.
+        
+        Input: self = Card object
+        Output: name of the image file of the card; string
+        """
+        
+        # initialise file_name
+        file_name = ""
+        
+        # add attribute color to file_name
+        if self.color == 0:
+            file_name += "green"
+        elif self.color == 1:
+            file_name += "purple"
+        elif self.color == 2:
+            file_name += "red"
+            
+        # add attribute shape to file_name
+        if self.shape == 0:
+            file_name += "diamond"
+        elif self.color == 1:
+            file_name += "oval"
+        elif self.color == 2:
+            file_name += "squiggle"
+        
+        # add attribute filling to file_name
+        if self.filling == 0:
+            file_name += "empty"
+        elif self.filling == 1:
+            file_name += "filled"
+        elif self.filling == 2:
+            file_name += "shaded"
+        
+        # add attribute number to file_name
+        file_name += str(self.number+1)
+        
+        # add necessary image type
+        file_name += ".gif"
+        
+        return file_name
