6d3a96c580595ea8a8806b7b3fa30cd84a537074
lgrvanlieshout <lgrlevi@gmail.com>
2021-07-01 10:55:32 +0200

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -54,16 +54,21 @@ def select_card(set_of_twelve, rect_set_of_twelve):
     
     Parameters
     ----------
-    set_of_twelve : List containing the twelve cards that are visible on the game table.
-    rect_set_of_twelve : List of Rectangles that are as big as the card images
-                         and that are on the same spot as the card images.
+    set_of_twelve : List 
+        List of the cards in the set of twelve cards on the table.
+    rect_set_of_twelve : list 
+        List of Rectangles that are as big as the card images and that are on 
+        the same spot as the card images.
 
     Returns
     -------
     index of mouse-clicked card
 
     """
+    # get position of mouse
     mouse_position = pygame.mouse.get_pos()
+    
+    # determine on which card is clicked
     for i in range(len(set_of_twelve)):
         if rect_set_of_twelve[i].collidepoint(mouse_position):
             return i
@@ -74,23 +79,35 @@ def select_card(set_of_twelve, rect_set_of_twelve):
 
 def possible_combinations(set_of_twelve: list):
     """
-    This function returns all the possible combinations of three different cards 
-    from a set of twelve cards, in which the order of the cards doesn't matter.
-    
-    input: list of the cards in the set of twelve cards
-    output: all possible combinations of three cards in this set of twelve cards.
+    This function returns all the possible combinations of three different 
+    indices from a list of indices, in which the order of the indices doesn't matter.
+
+    Parameters
+    ----------
+    set_of_twelve : list
+        List of the cards in the set of twelve cards on the table.
+
+    Returns
+    -------
+    possibilities : list
+        All possible combinations of three indices in a set of indices.
 
     """
+    #initialise possibilities
     possibilities = []
-    for i in range(12):
+    
+    # append combinations to possibilities
+    for i in range(len(set_of_twelve)):
         combination = [i]
-        for j in range(i+1,12):
+        for j in range(i+1,len(set_of_twelve)):
             combination.append(j)
-            for k in range(j+1,12):
+            for k in range(j+1,len(set_of_twelve)):
                 combination.append(k)
                 if combination not in possibilities and len(combination)==3:
                     possibilities.append(copy.copy(combination))
+                    # prepare for a new combination
                     combination.pop()
+            # prepare for a new combination
             combination.pop()
     return possibilities
 
@@ -100,15 +117,29 @@ def possible_combinations(set_of_twelve: list):
 
 def find_allsets(set_of_twelve: list):
     """
-    This function finds al Sets in a set of twelve cards.
-    Input: list of twelve cards
-    Output: If there is at least one Set, this function returns a list of all Sets (list of lists with the indices 
-            of the cards that form a Set).
-            If there isn't any Set in this set of twelve cards, the function
-            returns False.   
+    This function finds all Sets in a set.
+
+    Parameters
+    ----------
+    set_of_twelve : list
+        List of the cards in the set of twelve cards on the table.
+
+    Returns
+    -------
+    allsets : list/Boolean value
+        If there is at least one Set, this function returns a list of all Sets 
+        (list of lists with the indices of the cards that form a Set).
+        If there isn't any Set in this set of twelve cards, the function
+        returns False.   
+
     """
+    # initialise allsets
     allsets=[]
+    
+    # determine all possible combinations of indices
     Possible_combinations = possible_combinations(set_of_twelve)
+    
+    # check whether a combination is a Set
     for combination in Possible_combinations:
         card1 = set_of_twelve[combination[0]]
         card2 = set_of_twelve[combination[1]]
@@ -123,14 +154,23 @@ def find_allsets(set_of_twelve: list):
 
 def find_set(set_of_twelve: list):
     """
-    This function finds a Set in a set of twelve cards.
-    
-    Input: list of twelve cards
-    Output: If there is a Set, this function returns a list with the indices 
-            of the cards that form a Set.
-            If there isn't any Set in this set of twelve cards, the function
-            returns False.
+    This function finds a Set in a set.
+
+    Parameters
+    ----------
+    set_of_twelve : list
+        List of the cards in the set of twelve cards on the table.
+
+    Returns
+    -------
+    list/Boolean value
+        If there is a Set, this function returns a list with the indices 
+        of the cards that form a Set.
+        If there isn't any Set in this set of twelve cards, the function
+        returns False.
+
     """
+    # determines whether there is a set
     if find_allsets(set_of_twelve)==[]:
         return False
     else:
